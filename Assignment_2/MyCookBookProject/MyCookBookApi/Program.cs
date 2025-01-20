0be337d710f7d4578f243d46724c3cf467639b45
Ishmael Kwayisi <spyrorocks324@gmail.com>
2025-01-20 13:25:32 -0500

    Api Endpoint Added

--- Assignment_2/MyCookBookProject/MyCookBookApi/Program.cs
+++ Assignment_2/MyCookBookProject/MyCookBookApi/Program.cs
@@ -3,8 +3,16 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+builder.Services.AddControllers();
 
 var app = builder.Build();
+
+app.UseCors("AllowAll");
+app.MapControllers();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
--- Assignment_2/MyCookBookProject/MyCookBookApi/obj/Debug/net9.0/MyCookBookApi.AssemblyInfo.cs
+++ Assignment_2/MyCookBookProject/MyCookBookApi/obj/Debug/net9.0/MyCookBookApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCookBookApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db702bc68dfd845eef5af067e657f2d58d6d658b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7cfcb2d5f90e297a4f5514185e1ae1b6570c159")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCookBookApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCookBookApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
