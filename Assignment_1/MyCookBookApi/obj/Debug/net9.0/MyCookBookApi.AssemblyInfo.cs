31da409dabce0f1a2e803e56e054f500807c478c
Ishmael Kwayisi <spyrorocks324@gmail.com>
2025-01-30 20:28:07 -0500

    Bootstrap Theme Complete

--- Assignment_1/MyCookBookApi/obj/Debug/net9.0/MyCookBookApi.AssemblyInfo.cs
+++ Assignment_1/MyCookBookApi/obj/Debug/net9.0/MyCookBookApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCookBookApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+531cbd4acc49829e9a7c0809b7b252841cb5ae68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34f806590ce655eb3d430ebe808c65fb81e291b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCookBookApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCookBookApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
