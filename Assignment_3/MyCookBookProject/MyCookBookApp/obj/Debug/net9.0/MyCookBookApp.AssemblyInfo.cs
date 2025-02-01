3411611430043771966719784b4f8006283d2aa8
Ishmael Kwayisi <spyrorocks324@gmail.com>
2025-01-31 23:00:29 -0500

    More Tests

--- Assignment_3/MyCookBookProject/MyCookBookApp/obj/Debug/net9.0/MyCookBookApp.AssemblyInfo.cs
+++ Assignment_3/MyCookBookProject/MyCookBookApp/obj/Debug/net9.0/MyCookBookApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCookBookApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8d3e1360dadc17eabed8949ef8535c6f4361c54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecbfb5e4a728e9a46088f582e7c4c165b0cc816d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCookBookApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCookBookApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
