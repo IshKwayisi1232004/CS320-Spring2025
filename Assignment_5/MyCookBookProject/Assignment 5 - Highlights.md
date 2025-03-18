99b3b9efd4a36a34eefb6c7314e208bd4048b62c
Ishmael Kwayisi <spyrorocks324@gmail.com>
2025-03-17 20:58:47 -0400

    User can edit the recipes

--- Assignment_5/MyCookBookProject/Assignment 5 - Highlights.md
+++ Assignment_5/MyCookBookProject/Assignment 5 - Highlights.md
@@ -1,12 +1,13 @@
-Assignment #4
+Assignment #5
 
-Date: March 7, 2025
+Date: March 16, 2025
 
 Goals: 
+* Implement an edit and delete functionality for the user to have greater accessbility
 * Understand the seperation differences between the API and APP
 * Learn to fetch data from an API using HTTP client
 * Work with JSON serialization and API Responses
 * Improve the UI using Bootstrap and JavaScript
 
 Link to GitHub files: 
-https://github.com/IshKwayisi1232004/CS320-Spring2025/tree/main/Assignment_4
+https://github.com/IshKwayisi1232004/CS320-Spring2025/tree/main/Assignment_5
--- Assignment_5/MyCookBookProject/MyCookBookApi/obj/Debug/net9.0/MyCookBookApi.AssemblyInfo.cs
+++ Assignment_5/MyCookBookProject/MyCookBookApi/obj/Debug/net9.0/MyCookBookApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCookBookApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bd448a84fc762b528743953d7102bb3211d8fd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fc8431c90f39473bcdd217cdec78b46d439db28")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCookBookApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCookBookApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
